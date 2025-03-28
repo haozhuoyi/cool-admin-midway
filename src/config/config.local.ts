ce8d78412f9fefafb0f514f70a8bb14037ed4cc5
Hao Zhuoyi <72720742+haozhuoyi@users.noreply.github.com>
2025-03-28 14:10:49 +0800

    配置文件

--- src/config/config.local.ts
+++ src/config/config.local.ts
@@ -13,10 +13,10 @@ export default {
         host: '127.0.0.1',
         port: 3306,
         username: 'root',
-        password: '123456',
+        password: 'root',
         database: 'cool',
         // 自动建表 注意：线上部署的时候不要使用，有可能导致数据丢失
-        synchronize: true,
+        synchronize: false,
         // 打印日志
         logging: false,
         // 字符集
